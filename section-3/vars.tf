bc6c6fb50aeda1e7897196854857e24c5abedb7e
Jay Kim <tensaijskim1991@gmail.com>
2023-06-05 13:24:02 +0900

    Connecting to AWS using variables

---
+++ section-3/vars.tf
@@ -0,0 +1,23 @@
+variable "AWS_ACCESS_KEY" {
+
+}
+
+variable "AWS_SECRET_KEY" {
+
+}
+
+variable "AWS_SESSION_TOKEN" {
+
+}
+
+variable "AWS_REGION" {
+  default = "ap-northeast-1"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    ap-northeast-1 = "ami-0822295a729d2a28e"
+  }
+
+}
